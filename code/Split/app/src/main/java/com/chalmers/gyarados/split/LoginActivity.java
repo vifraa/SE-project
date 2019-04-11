6a14324ff0c36d40d05da043ff09a4f0b5c5af1d
Gustav Darhult <gustavdarhult@Gustavs-MacBook-Pro-2.local>
2019-04-11 11:30:27 +0200

    Added textbars, searchview and buttons in MainActivity

--- code/Split/app/src/main/java/com/chalmers/gyarados/split/LoginActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/LoginActivity.java
@@ -14,6 +14,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_view);
 
         final Button login = findViewById(R.id.googleLogInButton);
+        final Button fbLogin = findViewById(R.id.facebook_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
--- code/Split/app/src/main/java/com/chalmers/gyarados/split/MainActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/MainActivity.java
@@ -1,6 +1,7 @@
 package com.chalmers.gyarados.split;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -10,6 +11,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -44,6 +47,16 @@ public class MainActivity extends AppCompatActivity {
 
         getLocationPermission();
         updateLocation();
+
+        final Button findButton = findViewById(R.id.findbutton);
+
+        findButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     /**
