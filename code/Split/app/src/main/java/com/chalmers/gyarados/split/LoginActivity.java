8147997ace316e0bd882f7365ef353248d8140ca
Solen <alex.solberg@hotmail.com>
2019-04-11 09:20:53 +0200

    changed loginview name\

--- code/Split/app/src/main/java/com/chalmers/gyarados/split/LoginActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/LoginActivity.java
@@ -11,7 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_view);
 
         final Button login = findViewById(R.id.googleLogInButton);
 
