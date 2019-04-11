b8310d7d5160f1a683d7718be4b5c117548aa6c4
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2019-04-11 13:01:10 +0200

    Added map to mainview

--- code/Split/app/src/main/java/com/chalmers/gyarados/split/MainActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/MainActivity.java
@@ -9,23 +9,37 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 
+
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.List;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int DEFAULT_ZOOM = 15;
     private FusedLocationProviderClient fusedLocationClient; //Used to get last known position
     private Geocoder geocoder; //Used to get location from longitude and latitude
 
@@ -34,7 +48,13 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView positionTextView; //Textview to show the current location
 
+    private GeoDataClient geoDataClient;
 
+    private PlaceDetectionClient placeDetectionClient;
+
+    private GoogleMap mMap;
+    private Location mLastKnownLocation;
+    private LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +62,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_view);
 
         geocoder = new Geocoder(this, Locale.getDefault());
-        positionTextView = findViewById(R.id.latitudeTextView);
+        //positionTextView = findViewById(R.id.latitudeTextView);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        geoDataClient = Places.getGeoDataClient(this);
+        placeDetectionClient = Places.getPlaceDetectionClient(this);
 
-        getLocationPermission();
-        updateLocation();
+        //getLocationPermission();
+        //updateLocation();
 
-        final Button findButton = findViewById(R.id.findbutton);
-
-        findButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     /**
@@ -137,5 +152,66 @@ public class MainActivity extends AppCompatActivity {
             positionTextView.setText(noPermission);
         }
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        getLocationPermission();
+
+        updateLocationUI();
+
+        getDeviceLocation();
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (mLocationPermissionGranted) {
+                Task locationResult = fusedLocationClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            mLastKnownLocation = (Location) task.getResult();
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                        } else {
+                            //Log.d(TAG, "Current location is null. Using defaults.");
+                            //Log.e(TAG, "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch(SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void updateLocationUI() {
+        if(mMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                mLastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
     }
 }
