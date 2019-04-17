1c4d4511ffa2c0d769b537dd1632e9c3f3493e68
Viktor Franzén <Viktor_franzen@hotmail.com>
2019-04-17 10:08:53 +0200

    Init Spring Boot project

---
+++ code/split-backend/src/main/java/gyarados/splitbackend/SplitBackendApplication.java
@@ -0,0 +1,13 @@
+package gyarados.splitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitBackendApplication.class, args);
+	}
+
+}
