bf216e493922e055a621954b42a254eb3596baf6
Viktor Franzén <viktor.franzen@hotmail.com>
2019-04-22 14:41:33 +0200

    Added a group not found exception.

---
+++ code/split-backend/src/main/java/gyarados/splitbackend/Group/GroupNotFoundException.java
@@ -0,0 +1,12 @@
+package gyarados.splitbackend.Group;
+
+/**
+ * ChatMessageNotFoundException is an exeption thrown at runtime used when
+ * querying databases for ChatMessage where the specified id cannot be found in the database.
+ */
+public class GroupNotFoundException extends RuntimeException {
+
+    protected GroupNotFoundException(String id){
+        super("Could not find Group " + id);
+    }
+}
