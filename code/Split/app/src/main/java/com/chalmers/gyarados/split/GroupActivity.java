dcc970a7c0309c0c0a78f46f1d54acfd30fc3ddb
Oscar <Oscar@Thornets-MacBook-2.local>
2019-04-24 16:26:06 +0200

    Changed GroupActivity

--- code/Split/app/src/main/java/com/chalmers/gyarados/split/GroupActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/GroupActivity.java
@@ -2,12 +2,13 @@ package com.chalmers.gyarados.split;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class GroupActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.group_view);
+
     }
 }
