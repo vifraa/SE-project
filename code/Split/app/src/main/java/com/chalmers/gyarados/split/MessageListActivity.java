6f6e115b193bf3affd88357b2f24d04f802fb824
SpondonSiddiqui <spondonsiddiqui@gmail.com>
2019-05-02 09:42:19 +0200

    created class MessageListActivity, testing out LinearLayout

--- code/Split/app/src/main/java/com/chalmers/gyarados/split/MessageListActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/MessageListActivity.java
@@ -1,4 +1,23 @@
 package com.chalmers.gyarados.split;
 
-public class MessageListActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MessageListActivity extends AppCompatActivity {
+
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.group_view);
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageAdapter = new MessageListAdapter(this, messageList);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
