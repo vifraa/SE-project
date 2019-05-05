10bd3280a005721982b7e25afb53357f97f98017
Solen <alex.solberg@hotmail.com>
2019-05-05 21:52:50 +0200

    created test file for group

---
+++ code/split-backend/src/test/java/gyarados/splitbackend/group/GroupTest.java
@@ -0,0 +1,52 @@
+package gyarados.splitbackend.group;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GroupTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+        
+    }
+
+    @Test
+    public void addMessage() {
+    }
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void removeUser() {
+    }
+
+    @Test
+    public void getGroupId() {
+    }
+
+    @Test
+    public void getDirection() {
+    }
+
+    @Test
+    public void getMessages() {
+    }
+
+    @Test
+    public void getUsers() {
+    }
+
+    @Test
+    public void getMAX_GROUP_SIZE() {
+    }
+}
