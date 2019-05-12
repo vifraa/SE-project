f80c4115dfa5054da65dadd654ffb159ddab16df
SpondonSiddiqui <spondonsiddiqui@gmail.com>
2019-05-12 19:55:52 +0200

    initialized leavebuttons and confirm button

--- code/Split/app/src/main/java/com/chalmers/gyarados/split/ProfileActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/ProfileActivity.java
@@ -1,4 +1,18 @@
 package com.chalmers.gyarados.split;
 
-public class ProfileActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_view);
+
+        ImageButton leaveProfileButton = findViewById(R.id.leaveProfileButton);
+    }
 }
--- code/Split/app/src/main/java/com/chalmers/gyarados/split/RateActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/RateActivity.java
@@ -1,4 +1,19 @@
 package com.chalmers.gyarados.split;
 
-public class RateActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class RateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rate_view);
+
+        Button confirmButton = findViewById(R.id.confirmButton);
+        ImageButton leaveRateButton = findViewById(R.id.leaveProfileButton);
+    }
 }
