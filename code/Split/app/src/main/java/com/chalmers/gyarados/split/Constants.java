5964d3f278ac6c0abefdd34b7a187a9c9bb6f4d6
Viktor Franzén <viktor_franzen@hotmail.com>
2019-05-12 21:23:00 +0200

    New domain for deployed backend.

--- code/Split/app/src/main/java/com/chalmers/gyarados/split/Constants.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/Constants.java
@@ -4,7 +4,7 @@ public final class Constants {
     public static final String IP = "192.168.1.87";
     public static final String PORT = "8080";
     public static final Boolean develop = false;
-    public static final String deployedURL = "split-gyarados.herokuapp.com";
+    public static final String deployedURL = "split.frnzn.com";
     public static final int SUCCESS_RESULT = 0;
     public static final int FAILURE_RESULT = 1;
     private static final String PACKAGE_NAME =
