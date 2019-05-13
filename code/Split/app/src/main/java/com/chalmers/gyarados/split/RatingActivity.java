9a62e52f8667c0ddae9e1c5a94bc99820383974e
viktortorkelsson <torkelsson.viktor@gmail.com>
2019-05-13 11:16:51 +0200

    implemented listeners in onCreate

--- code/Split/app/src/main/java/com/chalmers/gyarados/split/RatingActivity.java
+++ code/Split/app/src/main/java/com/chalmers/gyarados/split/RatingActivity.java
@@ -1,5 +1,6 @@
 package com.chalmers.gyarados.split;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -11,7 +12,7 @@ import com.chalmers.gyarados.split.model.User;
 
 import java.util.List;
 
-public class RatingActivity extends AppCompatActivity {
+public class RatingActivity extends AppCompatActivity implements ClientListener {
     private Button rateConfirmButton;
     private RecyclerView ratingRecyclerView;
     private ImageButton leaveRateButton;
@@ -20,23 +21,72 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         rateConfirmButton = findViewById(R.id.confirmButton);
+        //disableConfirmButton();
         leaveRateButton = findViewById(R.id.leaveRateButton);
         ratingRecyclerView = findViewById(R.id.rating_recycler_view);
 
-        disableConfirmButton();
+        rateConfirmButton.setOnClickListener(v -> {
+            Intent intent = new Intent(RatingActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
 
+        leaveRateButton.setOnClickListener(v -> {
+            Intent intent = new Intent(RatingActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
 
-
-
+        ratingRecyclerView.setOnClickListener(v -> {
+            Intent intent = new Intent(RatingActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
 
         super.onCreate(savedInstanceState);
     }
 
-    private void disableConfirmButton() {
+    /*public void disableConfirmButton() {
         rateConfirmButton.setEnabled(false);
     }
-
+*/
     private void enableConfirmButton() {
         rateConfirmButton.setEnabled(true);
+    }
+
+    @Override
+    public void updateMembersList(List<User> users) {
+
+    }
+
+    @Override
+    public void newGroupMessageReceived(Message message) {
+
+    }
+
+    @Override
+    public void errorOnSubcribingOnTopic(Throwable throwable) {
+
+    }
+
+    @Override
+    public void errorOnLifeCycle(Throwable throwable) {
+
+    }
+
+    @Override
+    public void errorWhileSendingMessage(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onOldMessagesReceived(List<Message> messages) {
+
+    }
+
+    @Override
+    public void onClientReady() {
+
+    }
+
+
+    private void disableConfirmButton() {
     }
 }
